f3051a620476f96a53f2f51c48bf7e23fdfe302a
Hugh Davenport <hugh@davenport.net.nz>
2019-11-01 18:32:47 +1300

    Bunch of stuff, check commit
    * Fix bug where we weren't checking columns!
    * Add an agent that always loses
     * TEMP: if human playing, ai is losing agent
    * Added a toggle between random and best (Falsed out)
    * Track wins/loses/draws, as well as # and illegals (both players)
    * Add a worst net tracker, and use that half the time (COMMENTED OUT)
     * BUG: just noticed we do half if starting, and quarter if not (and not commented)
    * If opponent makes an illegal move X times, get the random agent to make one
    * Print stats if no losses

--- evolve-minimal.py
+++ evolve-minimal.py
@@ -9,10 +9,16 @@ DEBUG = False
 best_net = None
 best_fitness = None
 last_improved = -1
+is_random = True
+old_best_net = None
+worst_net = None
+worst_fitness = 1.0
+old_worst_net = None
+human = False
 
 EVALS_BEFORE_RANDOM = 1000
 EVALS_FOR_RANDOM = 1000
-NUMBER_OF_GAMES_TO_RUN = 50
+NUMBER_OF_GAMES_TO_RUN = 100
 
 def isValidMove(board, index):
     offset = int(len(board) / 2)
@@ -24,6 +30,9 @@ def isFinished(board):
         or isLine(board, 6, 7, 8) \
         or isLine(board, 0, 4, 8) \
         or isLine(board, 2, 4, 6) \
+        or isLine(board, 0, 3, 6) \
+        or isLine(board, 1, 4, 7) \
+        or isLine(board, 2, 5, 8) \
         or isDraw(board)
 
 def isDraw(board):
@@ -33,10 +42,10 @@ def isLine(board, i1, i2, i3):
     offset = int(len(board) / 2)
     if i1 < 0 or i2 < 0 or i3 < 0 or i1 >= offset or i2 >= offset or i3 >= offset:
         return False
-    if board[i1] == 0 or board[i1 + offset] == 0:
+    if board[i1] == 0.0 and board[i1 + offset] == 0.0:
         return False
-    return (board[i1] == board[i2] and board[i1] == board[i3]) or \
-            (board[i1 + offset] == board[i2 + offset] and board[i1 + offset] == board[i3 + offset])
+    return (board[i1] == 1.0 and board[i2] == 1.0 and board[i3] == 1.0) or \
+            (board[i1 + offset] == 1.0 and board[i2 + offset] == 1.0 and board[i3 + offset] == 1.0)
 
 def isLineWithEmptyGap(board, i1, i2, i3, player=True):
     offset = int(len(board) / 2)
@@ -60,6 +69,14 @@ def isWinner(board, player=True):
         return board[0 if player else 9] == 1.0
     if isLine(board, 2, 4, 6):
         return board[2 if player else 11] == 1.0
+    if isLine(board, 0, 3, 6):
+        return board[0 if player else 9] == 1.0
+    if isLine(board, 1, 4, 7):
+        return board[1 if player else 10] == 1.0
+    if isLine(board, 2, 5, 8):
+        return board[2 if player else 11] == 1.0
+        
+        
 
 def isAlmostWinner(board, player=True):
     return isLineWithEmptyGap(board, 0, 1, 2, player) \
@@ -99,31 +116,96 @@ class RandomAgent:
         output = [random.random() if index in choices else 0.0 for index in range(0, offset)]
         return output
 
-def eval_genome(genome, config, human=False):
-    global best_net, best_fitness, last_improved
+class LosingAgent:
+    def activate(self, board):
+        offset = int(len(board) / 2)
+        choices = [index for index in range(0, offset) if board[index] == 0.0 and board[index + offset] == 0.0]
+        bad = []
+        for choice in choices:
+            x = int(choice / 3)
+            y = int(choice % 3)
+            rows = [
+                board[x * 3:((x + 1) * 3) - 1],
+                board[(x * 3) + offset:((x + 1) * 3) - 1 + offset]
+            ]
+            columns = [
+                [board[y], board[y + 3], board[y + 6]],
+                [board[y + offset], board[y + 3 + offset], board[y + 6 + offset]]
+            ]
+            diagonals = []
+            if choice in [0, 4, 8]:
+                diagonals.append([board[0], board[4], board[8]])
+                diagonals.append([board[0 + offset], board[4 + offset], board[8 + offset]])
+            if choice in [2, 4, 6]:
+                diagonals.append([board[2], board[4], board[6]])
+                diagonals.append([board[2 + offset], board[4 + offset], board[6 + offset]])
+            if rows[0].count(1.0) == 2 or rows[1].count(1.0) == 2 or \
+                    columns[0].count(1.0) == 2 or columns[1].count(1.0) == 2:
+                bad.append(choice)
+            else:
+                for diagonal in diagonals:
+                    if diagonal.count(1.0) == 2:
+                        bad.append(choice)
+                        break
+        for choice in bad:
+            if len(choices) == 1:
+                # Let it make atleast one choice
+                break
+            choices.remove(choice)
+        output = [random.random() if index in choices else 0.0 for index in range(0, offset)]
+        return output
+
+def eval_genome(genome, config):
+    global best_net, best_fitness, last_improved, is_random, old_best_net, worst_net, worst_fitness, old_worst_net, human
     last_improved += 1
     genome.fitness = 0.0
     net = neat.nn.FeedForwardNetwork.create(genome, config)
+    if human:
+        net = LosingAgent()
     if best_net is None or last_improved > EVALS_BEFORE_RANDOM:
         if best_net is not None:
-            print("Haven't improved after a while, resetting to a random opponent, best so far", best_fitness)
+            if False:
+                if is_random:
+                    print("Haven't improved after a while, resetting to a old best, best so far", best_fitness, "worst so far", worst_fitness)
+                    best_net = old_best_net
+                    worst_net = old_worst_net
+                else:
+                    print("Haven't improved after a while, resetting to a random opponent and losing agent, best so far", best_fitness, "worst so far", worst_fitness)
+                    old_best_net = best_net
+                    old_worst_net = worst_net
+                    best_net = RandomAgent()
+                    worst_net = LosingAgent()
+                is_random = not is_random
         else:
             print("Using a random opponent")
-        best_net = RandomAgent()
+            best_net = RandomAgent()
+            worst_net = LosingAgent()
+            is_random = True
         last_improved = -EVALS_FOR_RANDOM
+    wins = [0, 0]
+    losses = [0, 0]
+    draws = [0, 0]
+    illegal = [0, 0]
+    opp_ill = [0, 0]
+    games = [0, 0]
+    offset = 9
+    unsure_board = [0 for i in range(0, offset)] * 2
     for g in range(0, NUMBER_OF_GAMES_TO_RUN): # Run a few games
         # two 3x3 boards, first is 1 if X has gone, second is if O has gone
-        offset = 9
         board = [0 for i in range(0, offset)] * 2
         playerFirst = random.random() > 0.5
+        games[0 if playerFirst else 1] += 1
         steps = []
         opp_steps = []
+        illegal_move = False
+        opp_ill_move = False
         while not isFinished(board):
             if playerFirst:
                 output = net.activate(board)
                 index = sorted(range(len(output)), key=lambda x: output[x])[-1]
                 if not isValidMove(board, index):
                     genome.fitness -= 10.0 / (1 + board.count(1.0)) # Penalty for doing wrong move, less later in the game
+                    illegal_move = True
                     if DEBUG:
                         print("Bad move from player")
                     break
@@ -141,8 +223,11 @@ def eval_genome(genome, config, human=False):
                 if human:
                     index = int(input("Enter an index: "))
                 else:
+                    opponent = best_net
+#                    if g < NUMBER_OF_GAMES_TO_RUN / 2:
+#                        opponent = worst_net
                     for i in range(0, 10): # Let opponent do bad move a few times if need be
-                        output = best_net.activate(board)
+                        output = opponent.activate(board)
                         index = sorted(range(len(output)), key=lambda x: output[x])[-1]
                         if not isValidMove(board, index):
                             index = None
@@ -154,7 +239,10 @@ def eval_genome(genome, config, human=False):
                 if index is None:
                     if DEBUG:
                         print("Several bad moves from opponent")
-                    break
+                    output = RandomAgent().activate(board)
+                    index = sorted(range(len(output)), key=lambda x: output[x])[-1]
+#                    opp_ill_move = True
+#                    break
 
                 board[index + offset] = 1.0
                 steps.append(index)
@@ -166,8 +254,15 @@ def eval_genome(genome, config, human=False):
                 if human:
                     index = int(input("Enter an index: "))
                 else:
+                    opponent = best_net
+                    if g < NUMBER_OF_GAMES_TO_RUN / 4:
+                        opponent = worst_net
+                    elif g < 2 * NUMBER_OF_GAMES_TO_RUN / 4:
+                        opponent = LosingAgent()
+                    elif g < 3 * NUMBER_OF_GAMES_TO_RUN / 4:
+                        opponent = RandomAgent()
                     for i in range(0, 10): # Let opponent do bad move a few times if need be
-                        output = best_net.activate(board)
+                        output = opponent.activate(board)
                         index = sorted(range(len(output)), key=lambda x: output[x])[-1]
                         if not isValidMove(board, index):
                             index = None
@@ -179,7 +274,10 @@ def eval_genome(genome, config, human=False):
                 if index is None:
                     if DEBUG:
                         print("Several bad moves from opponent")
-                    break
+                    output = RandomAgent().activate(board)
+                    index = sorted(range(len(output)), key=lambda x: output[x])[-1]
+#                    opp_ill_move = True
+#                    break
 
                 board[index + offset] = 1.0
                 steps.append(index)
@@ -194,6 +292,7 @@ def eval_genome(genome, config, human=False):
                 index = sorted(range(len(output)), key=lambda x: output[x])[-1]
                 if not isValidMove(board, index):
                     genome.fitness -= 10.0 / (1 + board.count(1.0)) # Penalty for doing wrong move, less later in the game
+                    illegal_move = True
                     if DEBUG:
                         print("Bad move from player")
                     break
@@ -215,6 +314,20 @@ def eval_genome(genome, config, human=False):
                 printBoard(board)
             else:
                 print("Someone played invalid move")
+        if isWinner(board, True):
+            wins[0 if playerFirst else 1] += 1
+        elif isWinner(board, False):
+            losses[0 if playerFirst else 1] += 1
+        elif isDraw(board):
+            draws[0 if playerFirst else 1] += 1
+        elif illegal_move:
+            illegal[0 if playerFirst else 1] += 1
+        elif opp_ill_move:
+            opp_ill[0 if playerFirst else 1] += 1
+        else:
+            #only reason would be if isFinished is true
+            print("setting unsure board, finished is", isFinished(board))
+            unsure_board = board
         genome.fitness += score(board, True)
 #        if not DEBUG and isAlmostWinner(board, True):
 #            print("Almost won, printing steps")
@@ -242,15 +355,47 @@ def eval_genome(genome, config, human=False):
 #                printBoard(board)
 #                player = not player
     genome.fitness = genome.fitness / NUMBER_OF_GAMES_TO_RUN
+    if losses[0] == 0 and losses[1] == 0 and illegal[0] == 0 and illegal[1] == 0:
+        print("lost none at all, and made no illegal moves, fitness", genome.fitness,
+                "played {} as first, {} as second".format(games[0], games[1]) + 
+                ", won {} ({}%) as first, {} ({}%) as second".format(wins[0], wins[1], wins[0] / games[0] if games[0] > 0 else 0) +
+                ", drew {} ({}%) as first, {} ({}%) as second player".format(draws[0], draws[1]) + 
+                ", opp ill moves {} ({}%), {} ({}%)".format(opp_ill[0], opp_ill[1])
+        )
     if DEBUG:
         print("Final fitness = {}".format(genome.fitness))
     if best_fitness is None:
         best_fitness = genome.fitness
+        if True:
+            best_net = net
     if last_improved >= 0 and genome.fitness > best_fitness and genome.fitness >= -1:
         print("Setting new opponent, fitness = {}, it was {}".format(genome.fitness, best_fitness))
         best_net = net
+        is_random = False
         best_fitness = genome.fitness
         last_improved = -1
+    elif genome.fitness > best_fitness:
+        # we still want this stat
+        print("Not yet worthy of a new opponent, but got better, fitness = {}, it was {}".format(genome.fitness, best_fitness))
+        best_fitness = genome.fitness
+        old_best_net = net
+        if True:
+            best_net = net
+    if last_improved >= 0 and is_random:
+        print("We finished our random period, switching to best found so far, which was {}, and worst was {}".format(best_fitness, worst_fitness))
+        best_net = old_best_net
+        if old_worst_net is not None:
+            worst_net = old_worst_net
+        is_random = False
+    if last_improved >= 0 and genome.fitness < worst_fitness and genome.fitness >= -1:
+        print("worst net opponent is {}, was {}".format(genome.fitness, worst_fitness))
+        worst_net = net
+        worst_fitness = genome.fitness
+    elif genome.fitness < worst_fitness and genome.fitness >= -1:
+        old_worst_net = net
+        worst_fitness = genome.fitness
+        if True:
+            worst_net = net
 
     return genome.fitness
         
@@ -284,7 +429,7 @@ p.add_reporter(stats)
 
 # Run until a solution is found.
 winner = None
-if True:
+if not human:
     if DEBUG:
         pe = neat.ParallelEvaluator(1, eval_genome)
     else:
